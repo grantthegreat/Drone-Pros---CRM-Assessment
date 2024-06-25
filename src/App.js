c8ab02847ecc18a36b7faea86e6f45754b4cba3f
Grant Fuellenbach <grant.fuellenbach@gmail.com>
2024-06-25 14:42:24 -0600

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CRMAssessment from './CRMAssessment';
+
+function App() {
+  return (
+    <div className="App">
+      <CRMAssessment />
+    </div>
+  );
+}
+
+export default App;
