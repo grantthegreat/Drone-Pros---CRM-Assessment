e990114a67f3e55a2d7019cceb37307541ad5232
Grant Fuellenbach <grant.fuellenbach@gmail.com>
2024-06-25 14:59:49 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+body {
+  background-color: #EDEEF0;
+}
