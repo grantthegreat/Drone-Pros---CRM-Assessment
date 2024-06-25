ec4e58af2c62e81f38a31357519e743d870bf12b
Grant Fuellenbach <grant.fuellenbach@gmail.com>
2024-06-25 14:43:10 -0600

    Create CRMAssessment.js

---
+++ src/src/CRMAssessment.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { ChevronRight, ChevronLeft } from 'lucide-react';
+
+const questions = [
+  // ... (question array as defined earlier)
+];
+
+const CRMAssessment = () => {
+  // ... (component code as defined earlier)
+};
+
+export default CRMAssessment;
